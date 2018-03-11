00aad66a815c900d7994e239fd5252eda7a364cb
RajPrabhakar <rajprabhakar1804@gmail.com>
2018-03-11 15:52:48 +0530

    Text to Speech enabled.

--- app/src/main/java/com/example/rajprabhakar/pointer/MainActivity.java
+++ app/src/main/java/com/example/rajprabhakar/pointer/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.rajprabhakar.pointer;
 
 import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +12,8 @@ import android.widget.TextView;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private CompoundButton autoFocus;
@@ -20,6 +23,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private static final int RC_BARCODE_CAPTURE = 9001;
     private static final String TAG = "BarcodeMain";
+    private String barcodeText;
+
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         autoFocus = findViewById(R.id.auto_focus);
         useFlash = findViewById(R.id.use_flash);
+
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if (i != TextToSpeech.ERROR) {
+                    tts.setLanguage(Locale.UK);
+                }
+            }
+        });
 
         findViewById(R.id.read_barcode).setOnClickListener(this);
     }
@@ -53,19 +68,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if (data != null) {
                     Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
                     statusMessage.setText(R.string.barcode_success);
-                    barcodeValue.setText(barcode.displayValue);
+                    barcodeText = barcode.displayValue;
+
+                    barcodeValue.setText(barcodeText);
+                    tts.speak(barcodeText, TextToSpeech.QUEUE_FLUSH, null);
                     Log.d(TAG, "Barcode read: " + barcode.displayValue);
                 } else {
                     statusMessage.setText(R.string.barcode_failure);
+                    barcodeText = statusMessage.getText().toString();
+                    tts.speak(barcodeText, TextToSpeech.QUEUE_FLUSH, null);
                     Log.d(TAG, "No barcode captured, intent data is null");
                 }
             } else {
                 statusMessage.setText(String.format(getString(R.string.barcode_error),
                         CommonStatusCodes.getStatusCodeString(resultCode)));
+                barcodeText = statusMessage.getText().toString();
+                tts.speak(barcodeText, TextToSpeech.QUEUE_FLUSH, null);
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
+    }
+
+    @Override
+    protected void onPause() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onStop();
     }
 }
 
